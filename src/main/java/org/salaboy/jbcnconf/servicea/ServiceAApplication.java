4a67edba99d687cd0adc621cfaa53b83e1f71ef2
salaboy <salaboy@gmail.com>
2019-05-13 08:23:42 +0100

    updating example-service-a scheduled requests

--- src/main/java/org/salaboy/jbcnconf/servicea/ServiceAApplication.java
+++ src/main/java/org/salaboy/jbcnconf/servicea/ServiceAApplication.java
@@ -3,14 +3,16 @@ package org.salaboy.jbcnconf.servicea;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class ServiceAApplication implements CommandLineRunner {
-
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceAApplication.class,
@@ -19,17 +21,35 @@ public class ServiceAApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(" -------------------------- ");
-        WebClient webClient = WebClient.builder().baseUrl("http://" + "service-b" + ".knativetutorial.svc.cluster.local").build();
-        webClient.get().uri("/").retrieve()
-                .bodyToMono(String.class).subscribe(System.out::print);
+        System.out.println(" --------- STARTED ---------- ");
 
     }
 
     @GetMapping
     public String sayHello() {
-        return "hi there from service A";
+        return "Hi there from service A";
     }
 
+
+    @Scheduled(fixedDelay = 5000)
+    public void callFunction() {
+        System.out.println(" --------------------------> Calling Function ");
+        WebClient webClient = WebClient.builder().baseUrl("http://" + "example-function-a" + ".knativetutorial.svc.cluster.local").build();
+        webClient.get().uri("/").retrieve()
+                .bodyToMono(String.class)
+                .doOnError(e -> System.out.println(">> Example Function A is not available, return a default answer"))
+                .subscribe(System.out::print);
+    }
+
+
+    @Scheduled(fixedDelay = 5000)
+    public void callServiceB() {
+        System.out.println(" --------------------------> Calling service ");
+        WebClient webClient = WebClient.builder().baseUrl("http://" + "service-b").build();
+        webClient.get().uri("/").retrieve()
+                .bodyToMono(String.class)
+                .doOnError(e -> System.out.println(">> Example Service B is not available, return a default answer"))
+                .subscribe(System.out::print);
+    }
 
 }
