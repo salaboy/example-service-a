d6a03b7762b72567831511d7cf00893dc67824aa
salaboy <salaboy@gmail.com>
2019-05-20 11:07:49 +0200

    refining example

--- src/main/java/org/salaboy/jbcnconf/servicea/ServiceAApplication.java
+++ src/main/java/org/salaboy/jbcnconf/servicea/ServiceAApplication.java
@@ -2,6 +2,7 @@ package org.salaboy.jbcnconf.servicea;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,6 +23,8 @@ public class ServiceAApplication implements CommandLineRunner {
     // Is the service On?
     private boolean on = false;
 
+    @Value("${my.application.name}:default-app")
+    private String applicationName;
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceAApplication.class,
@@ -34,7 +37,7 @@ public class ServiceAApplication implements CommandLineRunner {
         logger.info("> Service A is now: " + ((on) ? "ON" : "OFF"));
     }
 
-    @GetMapping()
+    @GetMapping
     public String sayHelloFromA() {
         return "Hi there from A";
     }
@@ -50,29 +53,45 @@ public class ServiceAApplication implements CommandLineRunner {
         return String.valueOf(on);
     }
 
-
+    //@TODO: function call per application
     @Scheduled(fixedDelay = 10000)
     public void callFunction() {
         if (on) {
-            logger.info(" --------------------------> Calling Function example-function-a.default.svc.cluster.local");
+            logger.info(" --- Function example-function-a --- ");
+            logger.info("\t >> Calling Downstream Function example-function-a.default.svc.cluster.local");
             WebClient webClient = WebClient.builder().baseUrl("http://" + "example-function-a.default.svc.cluster.local").build();
             webClient.get().uri("/").retrieve()
                     .bodyToMono(String.class)
-                    .doOnError(e -> logger.info(">> Example Function A is not available, return a default answer"))
-                    .subscribe(System.out::print);
+                    .doOnError(e -> {
+                        logger.info("\t >> Example Function A is NOT AVAILABLE.");
+                        logger.info(" --- End Error Function example-function-a --- \n ");
+                    })
+                    .subscribe(r -> {
+                        logger.info("\t >> Example Function A output: ");
+                        logger.info("\t " + r);
+                        logger.info(" --- End OK Function example-function-a --- \n");
+                    });
         }
     }
 
-
+    //@TODO: service-b call per application
     @Scheduled(fixedDelay = 10000, initialDelay = 5000)
     public void callServiceB() {
         if (on) {
-            logger.info(" --------------------------> Calling service-b ");
+            logger.info(" --- Service B --- ");
+            logger.info("\t >> Calling Downstream Service service-b");
             WebClient webClient = WebClient.builder().baseUrl("http://" + "service-b").build();
             webClient.get().uri("/").retrieve()
                     .bodyToMono(String.class)
-                    .doOnError(e -> logger.info(">> Example Service B is not available, return a default answer"))
-                    .subscribe(System.out::print);
+                    .doOnError(e -> {
+                        logger.info("\t >> Service B is NOT AVAILABLE.");
+                        logger.info(" --- End Error Service B --- \n ");
+                    })
+                    .subscribe(r -> {
+                        logger.info("\t >> Service B output: ");
+                        logger.info("\t " + r);
+                        logger.info(" --- End OK Service B --- \n");
+                    });
         }
     }
 
